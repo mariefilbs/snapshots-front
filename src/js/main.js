281f2508da2e6e82cdc4c7bdbc52bc0a7ec8275f
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-16 16:45:49 -0400

    Imported ng-cookies to main.js.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import angular from 'angular';
 import 'angular-ui-router';
+import 'angular-cookies';
 import Config from './config';
 import SERVER from './server';
 import UserController from './controllers/User';
 
 angular
-   .module('app', ['ui.router'])
+   .module('app', ['ui.router', 'ng-cookies'])
    .config(Config)
    .constant('SERVER', SERVER)
    .controller('UserController', UserController);
