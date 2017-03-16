321e8da2b2ac1a5ec57aaa4af131db37cd0ef8bb
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-16 16:51:29 -0400

    Added  and  to UserController.

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -1,7 +1,8 @@
 const SERVER = "https://intense-woodland-38140.herokuapp.com/";
 
-function UserController ($scope, $http, SERVER) {
+function UserController ($scope, $http, SERVER, $cookies, $state) {
   $scope.notifications = [];
+  console.log($cookies);
 
   $scope.removeMsg = (msg) => {
     var removed = $scope.notifications.filter(x => x != msg);
@@ -20,12 +21,13 @@ function UserController ($scope, $http, SERVER) {
 
   $scope.login = (user) => {
     $http.post(`${SERVER}/login`, user).then(resp => {
+      $cookies.put('access-token', resp.data.token);
       $state.go('/home');
     }).catch(error => {
        console.log(error);
     });
   }
 }
-UserController.$inject = ['$scope', '$http', 'SERVER'];
+UserController.$inject = ['$scope', '$http', 'SERVER', '$cookies', '$state'];
 
 export default UserController;
